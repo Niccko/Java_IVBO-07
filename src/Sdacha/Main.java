73ab7fee50f3eef82b139bc9ce90df0fd2e9a1ec
Niccko <mdroid308@gmail.com>
2020-09-17 17:06:56 +0300

    1

--- src/Sdacha/Main.java
+++ src/Sdacha/Main.java
@@ -3,9 +3,8 @@ package Sdacha;
 import java.util.Scanner;
 
 public class Main {
-    static int cnt = 0;
-
-    static void solve(int n) {
+    static int solve(int n) {
+        int cnt = 0;
         for (int i = n / 5; i >= 0; i--) {
             for (int j = (n - i * 5) / 3; j >= 0; j--) {
                 for (int k = n - j * 3 - i * 5; k >= 0; k--) {
@@ -13,12 +12,12 @@ public class Main {
                 }
             }
         }
+        return cnt;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        solve(n);
-        System.out.println(cnt);
+        System.out.println(solve(n));
     }
 }
