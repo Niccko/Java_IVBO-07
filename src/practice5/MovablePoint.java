1ae78c8add137cfffb6d957c3b74b3829e59dec0
Niccko <mdroid308@gmail.com>
2020-09-21 13:04:52 +0300

    1

--- src/practice5/MovablePoint.java
+++ src/practice5/MovablePoint.java
@@ -10,6 +10,38 @@ public class MovablePoint implements Movable {
         this.ySpeed = ySpeed;
     }
 
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(double xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public double getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(double ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+
     @Override
     public void moveUp() {
         y-=ySpeed;
