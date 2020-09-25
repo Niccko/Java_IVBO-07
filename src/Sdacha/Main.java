bac7b9b2a32aae801f2b00f9679304383896bb5c
Niccko <mdroid308@gmail.com>
2020-09-25 22:07:37 +0300

    К̴̧̗̳̥̙̗̱̜͖̮͉͂̃̊О̶̨̡̧̧͈͕͕̲̻̪͔͔̥̫͕̜͂̓͒̍̀̽͆͆̃͂̏͌ͅМ̶̮̲̜̼̮͆́̓̆̐͘̕͝М̵̣̩̋͗̑̓̂͒͊̀̓ͅͅЙ̷̡̢̧̧̭͕̫̪̻̪͓̘̱̼͔͕̩͛͐̊̒͑̒̒̏̀̔̃͝͝Т̸̳͉̪̖̖͙̻̫̤̬̐͜

--- src/Sdacha/Main.java
+++ src/Sdacha/Main.java
@@ -7,9 +7,7 @@ public class Main {
         int cnt = 0;
         for (int i = n / 5; i >= 0; i--) {
             for (int j = (n - i * 5) / 3; j >= 0; j--) {
-                for (int k = n - j * 3 - i * 5; k >= 0; k--) {
-                    if (i * 5 + j * 3 + k == n) cnt++;
-                }
+                cnt++;
             }
         }
         return cnt;
