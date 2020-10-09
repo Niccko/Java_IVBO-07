0b8348de96a0a00f5378626dd9dd3954f9f10233
Niccko <mdroid308@gmail.com>
2020-10-09 17:22:36 +0300

    К̴̧̗̳̥̙̗̱̜͖̮͉͂̃̊О̶̨̡̧̧͈͕͕̲̻̪͔͔̥̫͕̜͂̓͒̍̀̽͆͆̃͂̏͌ͅМ̶̮̲̜̼̮͆́̓̆̐͘̕͝М̵̣̩̋͗̑̓̂͒͊̀̓ͅͅЙ̷̡̢̧̧̭͕̫̪̻̪͓̘̱̼͔͕̩͛͐̊̒͑̒̒̏̀̔̃͝͝Т̸̳͉̪̖̖͙̻̫̤̬̐͜

--- src/practice10/App.java
+++ src/practice10/App.java
@@ -16,26 +16,49 @@ public class App extends JFrame {
     }
 
     public App(){
-        JPanel mainPanel = new JPanel();
-        JPanel buttonPanel = new JPanel(new FlowLayout());
+        Font mainFont = new Font("Arial",Font.BOLD,24);
+        DecimalFormat dc = new DecimalFormat("###.####");
+        Calculator calculator = new Calculator();
+        JPanel mainPanel = new JPanel(null);
+        JPanel buttonPanel = new JPanel(null);
 
         //Window setup
-        setTitle("App");
-        setSize(500,600);
+        setTitle("Calculator");
+        setSize(525,300);
         setResizable(false);
 
         //Creating elements
         JTextField textA = new JTextField(20);
+        textA.setBounds(10,10,480,50);
+        textA.setFont(mainFont);
+        textA.setToolTipText("Number A");
+
         JTextField textB = new JTextField(20);
-        JLabel result = new JLabel("Result");
+        textB.setBounds(10,70,480,50);
+        textB.setFont(mainFont);
+        textB.setToolTipText("Number B");
+
+        JTextField result = new JTextField();
+        result.setEnabled(false);
+        result.setDisabledTextColor(Color.BLACK);
+        result.setBounds(10,200,480,50);
+        result.setFont(mainFont);
+
+
         Vector<JButton> buttons = new Vector<>();
-        Calculator calculator = new Calculator();
-        DecimalFormat dc = new DecimalFormat("###.####");
 
         JButton addButton = new JButton("+");
+        addButton.setFont(mainFont);
+        addButton.setBounds(0,0,75,50);
         JButton subButton = new JButton("-");
+        subButton.setBounds(80,0,75,50);
+        subButton.setFont(mainFont);
         JButton multButton = new JButton("*");
+        multButton.setBounds(160,0,75,50);
+        multButton.setFont(mainFont);
         JButton divButton = new JButton("/");
+        divButton.setBounds(240,0,75,50);
+        divButton.setFont(mainFont);
 
         //Adding elements to the window
         buttons.add(addButton);
@@ -49,6 +72,7 @@ public class App extends JFrame {
         buttonPanel.add(multButton);
         buttonPanel.add(divButton);
         mainPanel.add(textB);
+        buttonPanel.setBounds(100,130,1000,50);
         mainPanel.add(buttonPanel);
         mainPanel.add(result);
 
@@ -61,6 +85,8 @@ public class App extends JFrame {
                     double a = Double.parseDouble(textA.getText());
                     double b = Double.parseDouble(textB.getText());
                     String res = dc.format(calculator.calculate(a,button.getText(),b));
+                    textA.setText("");
+                    textB.setText("");
                     result.setText(res);
                 }
             });
