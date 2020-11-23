2158eef831f7717918238d3c5611ae7038de75b6
Niccko <61467290+Niccko@users.noreply.github.com>
2020-11-23 21:37:45 +0300

    practice 23-24

---
+++ src/main/java/practice23/Main.java
@@ -0,0 +1,9 @@
+package practice23;
+
+public class Main {
+    public static void main(String[] args) {
+        Worker work = new Worker();
+        work.getTasks();
+        work.doTasks();
+    }
+}
