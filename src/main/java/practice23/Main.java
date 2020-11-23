5f85afe361bd6465075b8425fc1681fd468c65c5
Niccko <61467290+Niccko@users.noreply.github.com>
2020-11-23 21:53:08 +0300

    practice 23-24

--- src/main/java/practice23/Main.java
+++ src/main/java/practice23/Main.java
@@ -3,7 +3,14 @@ package practice23;
 public class Main {
     public static void main(String[] args) {
         Worker work = new Worker();
-        work.getTasks();
-        work.doTasks();
+        while(true) {
+            work.getTasks();
+            work.doTasks();
+            try {
+                Thread.sleep((int)(Math.random()*1000)+1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
